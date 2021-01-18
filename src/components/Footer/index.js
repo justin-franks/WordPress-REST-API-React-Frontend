f719252548b1c5d7e6073f80a3c005ff7ad6be74
Justin <mail@justinfranks.org>
2021-01-17 20:59:10 -0500

    Modified endpoint template for latest posts from DCstakeholders.Coop

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,14 +3,9 @@ import { FooterLayout } from "./styles";
 
 export const Footer = () => (
   <FooterLayout className="site-footer">
-    <p className="site-info">
-      &copy; {new Date().getFullYear()} Greg Rickaby{" "}
-      <span role="img" aria-label="coffee">
-        ☕
-      </span>
-    </p>
+    <p className="site-info">{new Date().getFullYear()} | Co-OS </p>
     <p>
-      <a href="https://github.com/gregrickaby/React-WordPress-API-Demo">
+      <a href="https://github.com/CrowdWork/React-WordPress-REST-API-Sandbox.git">
         View this on Github
       </a>
     </p>
